9e3c1f1deca698368d2c7e4d46c60f174a099bc3
Jehu <jehucjp@gmail.com>
2019-01-21 14:35:05 -0500

    first commit

---
+++ PrivateChat/src/main/java/com/privatechat/PrivateChatApplication.java
@@ -0,0 +1,14 @@
+package com.privatechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateChatApplication.class, args);
+	}
+
+}
+
